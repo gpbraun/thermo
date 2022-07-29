5c85e84b988ab06747a0d97f679e5163cf3f9ba5
Gabriel Braun <braun.pineschi@gmail.com>
2022-07-29 11:02:57 -0300

    Suporte para diferentes pólvoras

--- streamlit_thermo.py
+++ streamlit_thermo.py
@@ -5,14 +5,25 @@ Cálculo de temperatura adiabática de chama
 import streamlit as st
 import cantera as ct
 import pandas as pd
+import json
 
 st.title('LQM ICT')
 
 st.header('Composição')
 
-composition = st.radio('Pólvora', ['IMBEL BD 528'])
+with open('data/compositions.json', 'r') as json_file:
+        compositions = json.load(json_file)
 
-composition_dict = {'NC': 56.50, 'NG': 39.8, 'DBP': 1.40, 'EC': 2.30, 'C': 0.05}
+options = [comp for comp in compositions]
+options.append('Outro')
+
+composition = st.radio('Pólvora', options)
+
+if composition == 'Outro':
+    st.file_uploader('Arquivo CSV')
+    st.experimental_rerun()
+else:
+    composition_dict = compositions[composition]['composition']
 
 st.header('Parâmetros')
 
@@ -24,6 +35,8 @@ m = float(st.number_input('Massa m/g', value = 2.5)) * 1e-3
 bd = ct.Solution('data/thermodat.yaml')
 bd.TDY = 300, m/V, composition_dict
 
+U0 = bd.int_energy_mass
+
 st.header('Resultados')
 
 bd.equilibrate('UV', max_steps=1e5)
@@ -32,8 +45,13 @@ q = ct.Quantity(bd, mass=m)
 
 P = bd.P/1e6
 
+U = bd.int_energy_mass
+
+dU = (U - U0) * 1000 / 4.2
+
 st.text(f'Temperatura: {bd.T:.2f} K')
 st.text(f'Pressão: {P:.2f} MPa')
+# st.text(f'Calor de combustão: {dU} cal/g')
 
 st.header('Produtos de Combustão')
 
@@ -41,8 +59,14 @@ fractions = bd.mass_fraction_dict(1e-4)
 mole_fractions = bd.mole_fraction_dict(1e-4)
 
 for substance, mass_fraction in fractions.items():
-    fractions[substance] = [mass_fraction, mole_fractions[substance]]
+    try:
+        fractions[substance] = [mass_fraction, mole_fractions[substance]]
+    except KeyError:
+        pass
 
 df = pd.DataFrame.from_dict(fractions, orient='index', columns=['Fração mássica', 'Fração molar'])
 
 st.bar_chart(df)
+
+    
+
