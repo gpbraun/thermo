4f6ff1e7d6968a570700850e9b93345f62d01ac3
Gabriel Braun <braun.pineschi@gmail.com>
2022-07-29 08:35:15 -0300

    Gráfico de barras

--- streamlit_thermo.py
+++ streamlit_thermo.py
@@ -10,9 +10,9 @@ st.title('LQM ICT')
 
 st.header('Composição')
 
-st.radio('Pólvora', ['IMBEL BD 528'])
+composition = st.radio('Pólvora', ['IMBEL BD 528'])
 
-composition = {'NC': 56.50, 'NG': 39.8, 'DBP': 1.40, 'EC': 2.30, 'C': 0.05}
+composition_dict = {'NC': 56.50, 'NG': 39.8, 'DBP': 1.40, 'EC': 2.30, 'C': 0.05}
 
 st.header('Parâmetros')
 
@@ -22,7 +22,7 @@ V = int(st.number_input('Volume V/cm3', value = 100)) * 1e-6
 m = int(st.number_input('Massa m/g', value = 2.5)) * 1e-3
 
 bd = ct.Solution('data/thermodat.yaml')
-bd.TDY = 300, m/V, composition
+bd.TDY = 300, m/V, composition_dict
 
 st.header('Resultados')
 
@@ -35,6 +35,14 @@ P = bd.P/1e6
 st.text(f'Temperatura: {bd.T:.2f} K')
 st.text(f'Pressão: {P:.2f} MPa')
 
-st.header('Produtos de Reação')
+st.header('Produtos de Combustão')
 
-st.json(bd.mass_fraction_dict())
+fractions = bd.mass_fraction_dict(1e-4)
+mole_fractions = bd.mole_fraction_dict(1e-4)
+
+for substance, mass_fraction in fractions.items():
+    fractions[substance] = [mass_fraction, mole_fractions[substance]]
+
+df = pd.DataFrame.from_dict(fractions, orient='index', columns=['Fração mássica', 'Fração molar'])
+
+st.bar_chart(df)
